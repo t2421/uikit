1b86f4cd811409817c7d9431ff65e8c0570d73c8
Taiga Kiyotaki <t2421gm@gmail.com>
2019-01-29 01:13:52 +0900

    init

---
+++ uiLabel/uiLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiLabel
+//
+//  Created by taigakiyotaki on 2019/01/29.
+//  Copyright © 2019 taigakiyotaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ uiLabel/uiLabelTests/uiLabelTests.swift
@@ -0,0 +1,34 @@
+//
+//  uiLabelTests.swift
+//  uiLabelTests
+//
+//  Created by taigakiyotaki on 2019/01/29.
+//  Copyright © 2019 taigakiyotaki. All rights reserved.
+//
+
+import XCTest
+@testable import uiLabel
+
+class uiLabelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
